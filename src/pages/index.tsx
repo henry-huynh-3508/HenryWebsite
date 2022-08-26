b23cda5289f0ffea21fbcd3a4b101112ef0a0dbf
Mint <mintnguyen.pa@gmail.com>
2022-08-26 13:10:33 -0700

    init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+// @mui
+import { styled } from '@mui/material/styles';
+// layouts
+import Layout from '../layouts';
+// components
+import Page from '../components/Page';
+// sections
+import {
+  HomeHero,
+  HomeMinimal,
+  HomeDarkMode,
+  HomeLookingFor,
+  HomeColorPresets,
+  HomePricingPlans,
+  HomeAdvertisement,
+  HomeCleanInterfaces,
+  HomeHugePackElements,
+} from '../sections/home';
+
+// ----------------------------------------------------------------------
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  overflow: 'hidden',
+  position: 'relative',
+  backgroundColor: theme.palette.background.default,
+}));
+
+// ----------------------------------------------------------------------
+
+HomePage.getLayout = function getLayout(page: React.ReactElement) {
+  return <Layout variant="main">{page}</Layout>;
+};
+
+// ----------------------------------------------------------------------
+
+export default function HomePage() {
+  return (
+    <Page title="Henry H. - Tech Lead">
+      <HomeHero />
+
+      <ContentStyle>
+        <HomeMinimal />
+
+        <HomeHugePackElements />
+
+        <HomeDarkMode />
+
+        <HomeColorPresets />
+
+        <HomeCleanInterfaces />
+
+        <HomePricingPlans />
+
+        <HomeLookingFor />
+
+        <HomeAdvertisement />
+      </ContentStyle>
+    </Page>
+  );
+}
